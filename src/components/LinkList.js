86431e00d9eba2fdc1fa2a0595ff731655987d9b
Idan <idan.zonshein@powerhrg.com>
2020-08-21 11:48:05 -0400

    Realtime Updates using subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -25,36 +25,57 @@ export const FEED_QUERY = gql`
     }
   }
 `
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
 
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 
 
 class LinkList extends Component {
-  render() {
-    return (
-      <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
-
-          const linksToRender = data.feed.links
-
-          return (
-            <div>
-              {linksToRender.map((link, index) => (
-                <Link
-                key={link.id}
-                link={link}
-                index={index}
-                updateStoreAfterVote={this._updateCacheAfterVote}
-              />
-              ))}
-            </div>
-          )
-        }}
-      </Query>
-    )
-  }
   _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
 
@@ -62,6 +83,61 @@ class LinkList extends Component {
     votedLink.votes = createVote.link.votes
 
     store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data, subscribeToMore }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
+
+          const linksToRender = data.feed.links
+
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
+            </div>
+          )
+        }}
+      </Query>
+    )
   }
 }
 
