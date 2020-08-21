89d52be11f1ef1ce561b6292b2d2b64fb84530ca
Idan <idan.zonshein@powerhrg.com>
2020-08-21 11:03:38 -0400

    Update the store with right information

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { FEED_QUERY } from './LinkList'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -41,13 +42,21 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-          <Mutation
-            mutation={POST_MUTATION}
-            variables={{ description, url }}
-            onCompleted={() => this.props.history.push('/')}
-          >
-            {postMutation => <button onClick={postMutation}>Submit</button>}
-          </Mutation>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/')}
+          update={(store, { data: { post } }) => {
+            const data = store.readQuery({ query: FEED_QUERY })
+            data.feed.links.unshift(post)
+            store.writeQuery({
+              query: FEED_QUERY,
+              data
+            })
+          }}
+        >
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
       </div>
     )
   }
